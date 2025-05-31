442cebc210847ca316d5a9110591efa67f4b1948
Bhanushankar369 <141208242+Bhanushankar369@users.noreply.github.com>
2025-05-31 20:25:57 +0530

    Ready for deployment

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-x#-xhyw%+nq))+n3b&gz6o#&3gej--739y567#^$kgrm#=!h66
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yourusername.pythonanywhere.com']
 
 
 # Application definition
